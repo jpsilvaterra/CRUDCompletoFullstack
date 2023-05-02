31dae2fc57f43aea95ba2878953f406fda58cde5
João Pedro Silva Terra <101167595+jpsilvaterra@users.noreply.github.com>
2023-05-02 01:38:27 -0300

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products.Api.Data;
+using Products.Api.Models;
+
+namespace Products.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductsContext _context;
+        public ProductsController(ProductsContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("products")]
+        public async Task<ActionResult> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+        [HttpPost]
+        [Route("/products")]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+           await _context.Products.AddAsync(product);
+           await _context.SaveChangesAsync();
+
+           return Ok(product);
+        }
+
+        [HttpPut]
+        [Route("/products")]
+        public async Task<ActionResult> UpdateProduct(Product product)
+        {
+            var dbProduct = await _context.Products.FindAsync(product.id);
+
+            if (dbProduct == null)
+                return NotFound();
+
+            dbProduct.Name = product.Name;
+            dbProduct.Price= product.Price;
+            dbProduct.Category = product.Category;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("/products")]
+        public async Task<ActionResult> UpdateProduct(Guid id)
+        {
+            var dbProduct = await _context.Products.FindAsync(id);
+
+            if (dbProduct == null)
+                return NotFound();
+
+            _context.Products.Remove(dbProduct);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
